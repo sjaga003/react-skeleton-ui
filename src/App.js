5cec4d796fa4fa2fd41cccfe3e83af2c615a5ce9
Suhas Jagannath <sjaga003@ucr.edu>
2021-05-11 15:43:48 -0700

    Base styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import Articles from './components/Articles';
+import User from './components/User';
+
 function App() {
   return (
     <div className="App">
       <header>
         <h1>React Skeletons</h1>
       </header>
-      <div className="content"></div>
+      <div className="content">
+        <Articles /> <User />
+      </div>
     </div>
   );
 }
